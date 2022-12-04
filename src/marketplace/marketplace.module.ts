279d2f18141cd475b8d5edd5da6f7999a96cf201
ScriptSavior <laoroney1113@gmail.com>
2022-12-05 02:11:02 +0900

    downgrade: moongoose version

--- src/marketplace/marketplace.module.ts
+++ src/marketplace/marketplace.module.ts
@@ -4,13 +4,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Profile, ProfileSchema } from './profile/profile.schema';
 import { ProfileService } from './profile/profile.service';
 import { ProfileController } from './profile/profile.controller';
+import { NftService } from './nft/Nft.service';
+import { NftController } from './nft/nft.controller';
+import { Nft, NftSchema } from './nft/nft.schema';
 
 @Module({
-  providers: [ProfileService],
-  controllers: [ProfileController],
+  providers: [ProfileService, NftService],
+  controllers: [ProfileController, NftController],
   imports: [
     MongooseModule.forFeature(
-      [{ name: Profile.name, schema: ProfileSchema }],
+      [
+        { name: Profile.name, schema: ProfileSchema },
+        { name: Nft.name, schema: NftSchema },
+      ],
       'test',
     ),
   ],
